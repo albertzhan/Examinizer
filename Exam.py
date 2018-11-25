395494a206d42c24df72d6617cceb3e5e9a20ff0
Albert Zhan <albertzhan@gmail.com>
2018-11-24 20:28:19 -0800

    corrections in exam.py

--- Exam.py
+++ Exam.py
@@ -1,9 +1,8 @@
 class Exam:
-    def __init__(self, link, year, term, pdf_exam, pdf_solution, instructor, course = "CS61a"):
-        self.link = link
-        self.year = year
-        self.term = term
-        self.pdf_exam = pdf_exam
-        self.pdf_solution = pdf_solution
+    def __init__(self, instructor, term, exam_type, pdf_exam, pdf_solution, course = "CS61a"):
         self.instructor = instructor
+        self.term = term
+        self.exam_type = exam_type
+        self.pdf_exam = pdf_exam #these are links to the pdf
+        self.pdf_solution = pdf_solution
         self.course = course
