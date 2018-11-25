949a5d44fcd5d33fdadd83781c5cfbc9d2101360
Andy Zou <andyzou_jiaming@berkeley.edu>
2018-11-24 21:43:22 -0800

    Update Exam.py

--- Exam.py
+++ Exam.py
@@ -1,3 +1,5 @@
+import PyPDF2
+
 class Exam:
     def __init__(self, instructor, term, exam_type, pdf_exam, pdf_solution, course = "CS61a"):
         self.instructor = instructor
@@ -6,3 +8,20 @@ class Exam:
         self.pdf_exam = pdf_exam #these are links to the pdf
         self.pdf_solution = pdf_solution
         self.course = course
+        self.content = ''
+
+        pdfFileObj = open(self.pdf_exam, 'rb')
+        self.pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+        self.num_pages = self.pdfReader.numPages
+        self.get_text()
+
+
+    def get_text(self):
+    	self.page1_text = self.pdfReader.getPage(0).extractText()
+    	page = 1
+    	while page < num_pages:
+    		self.content += self.pdfReader.getPage(page).extractText()
+    		page += 1
+
+    def search_string(self, str):
+    	return str in self.content
