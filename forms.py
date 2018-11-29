63141f507f4172979cadf441a48ca7639797d56f
Maxwell Lee <maxdae.lee@gmail.com>
2018-11-28 20:11:39 -0800

    not finished

--- forms.py
+++ forms.py
@@ -1,6 +1,15 @@
 from wtforms import Form, StringField, SelectField
 
+
+
+
 class SearchForm(Form):
-    choices = [('CS61a', 'CS61a'), ('CS70', 'CS70')]
+    connection = sqlite3.Connection('data.db')
+    cursor = connection.cursor()
+
+    cursor.execute(".tables")
+    tables = cursor.fetchall()
+    
+    choices = [('cs61a', 'cs61a'), ('cs70', 'cs70')]
     select = SelectField('Search Through Class:', choices=choices)
     search = StringField('')
